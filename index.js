62c941056fc579f60038b5893479dda5c149949b
kennedfer <kenned14santos@gmail.com>
2023-05-05 16:16:33 -0300

    testando com linhas de texto

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ app.get("/", (req, res) => {
   res.setHeader("Content-Type", "image/svg+xml");
 
   res.send(`
-  <svg width="100" height="120" viewBox="0 0 100 120" fill="#858585" xmlns="http://www.w3.org/2000/svg">
-  <style>
-  .text { font: 600 16px 'Segoe UI', Ubuntu, Sans-Serif; fill: #2F80ED }
-  .small { font: 600 12px 'Segoe UI', Ubuntu, Sans-Serif; fill: #252525 }
-  .gray { fill: #858585 }
-  </style>
+  <svg viewBox="0 0 240 80" xmlns="http://www.w3.org/2000/svg">
+    <style>
+      .small {
+        font: italic 13px sans-serif;
+      }
+      .heavy {
+        font: bold 30px sans-serif;
+      }
+    </style>
+
+    <text x="10" y="20" style="fill:red;">Several lines:
+      <tspan x="10" y="45">First line.</tspan>
+      <tspan x="10" y="70">Second line.</tspan>
+    </text>
   </svg>
   `);
   console.log("entrou");
