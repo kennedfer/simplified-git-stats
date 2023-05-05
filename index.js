4fe94307fc57423266e5b34c5a49c4a74a66fa18
kennedfer <kenned14santos@gmail.com>
2023-05-05 11:38:44 -0300

    envia svg de exemplo

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(`
+    <svg height="100" width="100">
+      <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+    </svg>
+  `);
   console.log("entrou");
 });
 
